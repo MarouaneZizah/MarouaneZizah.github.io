d2ae1edf42dc7cbb346fc755708d87137b612d71
Marouane Zizah <m.zizah@mn-agency.com>
2017-12-10 23:32:40 +0000

    Social Media Links, Contact Section, Work Section & Responsive Fix

--- js/script.js
+++ js/script.js
@@ -1,54 +1,48 @@
+$('.sliderDetails').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    draggable :false,
+    swipe : false,
+    touchMove : false,
+    pauseOnHover : false,
+    asNavFor: '.mainSlider'
+});
 
-$(document).ready(function(){
-
-    $('.sliderDetails').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false,
-        fade: true,
-        draggable :false,
-        swipe : false,
-        touchMove : false,
-        pauseOnHover : false,
-        asNavFor: '.mainSlider'
-    });
-
-
-    $(".mainSlider").slick({
-        autoplay : true,
-        speed : 2000,
-        autoplaySpeed: 3000,
-        infinite: true,
-        asNavFor: '.sliderDetails',
-        arrows : false,
-        dots: false,
-        draggable :true,
-        swipe : true,
-        touchMove : false,
-        pauseOnHover : false,
-        centerMode: true, 
-        slidesToShow:3,
-        slidesToScroll: 1,
-        responsive : true,
-        responsive: 
-        [
+$(".mainSlider").slick({
+    autoplay : true,
+    speed : 2000,
+    autoplaySpeed: 3000,
+    infinite: true,
+    asNavFor: '.sliderDetails',
+    arrows : false,
+    dots: false,
+    draggable :false,
+    swipe : false,
+    touchMove : false,
+    pauseOnHover : false,
+    centerMode: true, 
+    slidesToShow:3,
+    slidesToScroll: 1,
+    responsive : true,
+    responsive: 
+    [
+        {
+            breakpoint: 768,
+            settings: 
             {
-                breakpoint: 768,
-                settings: 
-                {
-                    centerPadding: '40px',
-                    slidesToShow: 2
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: 
-                {
-                    centerPadding: '40px',
-                    slidesToShow: 1
-                }
+                centerPadding: '40px',
+                slidesToShow: 2
             }
-        ]
-    });
-
+        },
+        {
+            breakpoint: 480,
+            settings: 
+            {
+                centerPadding: '40px',
+                slidesToShow: 1
+            }
+        }
+    ]
 });
