db7fae76d84ef4c855521d3d52b24f21b2458469
Marouane Zizah <m.zizah@mn-agency.com>
2017-12-24 00:35:06 +0000

    scroll update

--- js/script.js
+++ js/script.js
@@ -47,12 +47,6 @@ $(".mainSlider").slick({
     ]
 });
 
-$(document).on('click', 'a[href^="#"]', function (event) 
-{
-    event.preventDefault();
-    $('html, body').animate({scrollTop: $($.attr(this, 'href')).offset().top - 80 }, 'slow');
-});
-
 $(window).scroll(function()
 {    
     var a = 112;
@@ -65,4 +59,63 @@ $(window).scroll(function()
     {
         $("nav").removeClass('scrollNav');
     }
+});
+
+var lastId,
+    topMenu = $("ul.navbar-nav"),
+    topMenuHeight = topMenu.outerHeight()+1,
+    menuItems = topMenu.find("a"),
+    scrollItems = menuItems.map(function()
+    {
+        var item = $($(this).attr("href"));
+        if (item.length) { return item; }
+    });
+
+$(document).on('click', 'a[href^="#"]', function (event) 
+{
+    event.preventDefault();
+    $('html, body').animate({scrollTop: $($.attr(this, 'href')).offset().top - 40 }, 850, 'swing');
+});
+
+/*
+menuItems.click(function(e)
+{
+    var href = $(this).attr("href"),
+        offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
+    
+    $('html, body').stop().animate({ 
+      scrollTop: offsetTop
+    }, 850);
+    e.preventDefault();
+});
+*/
+
+
+$(window).scroll(function()
+{
+    var fromTop = $(this).scrollTop()+topMenuHeight;
+   
+    var cur = scrollItems.map(function()
+    {
+        if ($(this).offset().top < fromTop) return this;
+    });
+
+    cur = cur[cur.length-1];
+    var id = cur && cur.length ? cur[0].id : "";
+   
+    if (lastId !== id) 
+    {
+       lastId = id;
+        
+       menuItems.removeClass("active").filter("[href='#"+id+"']").addClass("active");
+
+       //menuItems.parent().removeClass("active").end().filter("[href='#"+id+"']").parent().addClass("active");
+    }    
+
+    var scrollHeight = $(document).height(),
+	    scrollPosition = $(window).height() + $(window).scrollTop();
+    if ((scrollHeight - scrollPosition) / scrollHeight === 0) 
+    {
+        menuItems.removeClass("active").filter("[href='#contactSection']").addClass("active");
+	}
 });
